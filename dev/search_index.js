db540fcb3e34400373d7d3c53260b6ad21bdc49e
Documenter.jl <documenter@juliadocs.github.io>
2022-11-13 11:48:56 +0000

    build based on f8d5fda

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TrixiTestCI","category":"page"},{"location":"#TrixiTestCI","page":"Home","title":"TrixiTestCI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TrixiTestCI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TrixiTestCI]","category":"page"}]
+}
